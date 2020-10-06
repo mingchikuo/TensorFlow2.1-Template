3054244ed2cdde17afb78ef577db5c7e4d04ee88
Ming-Chi Kuo <mingchikuo7@gmail.com>
2020-10-06 17:34:47 +0800

    commit

---
+++ Baseline/Baseline.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.keras import Model
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Baseline(Model):
+    def __init__(self):
+        super(Baseline, self).__init__()
+        self.c1 = Conv2D()
+        self.b1 =
+        self.a1 =
+        self.p11 =
+        self.d11 =
