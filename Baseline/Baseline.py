6cb6ebe4a541bfe0ea463c6d08d670fe4ac15c63
Ming-Chi Kuo <mingchikuo7@gmail.com>
2020-10-06 17:42:24 +0800

    commit

--- Baseline/Baseline.py
+++ Baseline/Baseline.py
@@ -8,8 +8,28 @@ from matplotlib import pyplot as plt
 class Baseline(Model):
     def __init__(self):
         super(Baseline, self).__init__()
-        self.c1 = Conv2D()
-        self.b1 =
-        self.a1 =
-        self.p11 =
-        self.d11 =
+        self.c1 = Conv2D(filters=6, kernel_size=(5, 5), padding='same')
+        self.b1 = BatchNormalization()
+        self.a1 = Activation('relu')
+        self.p1 = MaxPooling2D(pool_size=(2, 2), strides=2, padding='same')
+        self.d1 = Dropout(0.2)
+
+        self._flatten = Flatten()
+        self.f1 = Dense(128, activation='relu')
+        self.d2 = Dropout(0.2)
+        self.f2 = Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.c1(x)
+        x = self.b1(x)
+        x = self.a1(x)
+        x = self.p1(x)
+        x = self.d1(x)
+        x = self.flatten(x)
+        x = self.f1(x)
+        x = self.d2(x)
+        x = self.f2(x)
+        return y
+
+model = Baseline()
+
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## 六步法
 1. import
 2. train, test
-3. model.tf.keras.models.Sequential
+3. Model
 4. model.compile
 5. model.fit
 6. model.summary
